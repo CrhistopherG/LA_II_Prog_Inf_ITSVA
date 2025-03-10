631190456f81c5aa6d12706b80f0fc8fe9b5dd5e
EXMOKKING <juanpabloperezmartin0@gmail.com>
2025-03-09 18:44:27 -0600

    Ejercicios de juanpa agregados
    Ejercicios de python, java y c++ agregados

---
+++ Java/Factorial.java
@@ -0,0 +1,21 @@
+// Escribe un programa que calcule el factorial de un número dado.
+
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        Scanner f = new Scanner(System.in); 
+        System.out.print("Ingrese cualquier numero: "); // Solicita un numero
+        int numero = f.nextInt(); // Lee el numero
+
+        long factorial = 1; // Inicializa la variable factorial en 1
+        for (int i = 1; i <= numero; i++) { // Ciclo for para calcular el factorial 
+            factorial *= i; // Multiplica el factorial por el numero actual
+        }
+
+        System.out.println("El factorial de " + numero + " es " + factorial); 
+        f.close(); 
+    }
+}
+
+// javap -c Factorial
